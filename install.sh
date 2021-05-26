6c0a3d266bf2246f2eb34682fd2ebca42a0afbaf
takrim <takrimulalom@gmail.com>
2021-05-27 04:54:15 +0600

    same as previous

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 echo "installing Shell FB..."
 sudo cp fb /usr/bin/
 sudo chgrp $(whoami) /usr/bin/fb
-sudo chmod 770 /usr/bin/fb
+sudo chmod 750 /usr/bin/fb
 echo "Installation Succesful"
 echo "run fb to run Shell Facebook"
