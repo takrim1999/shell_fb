e5f123df361c188f8658c7b81304137d4c511bce
takrim <takrimulalom@gmail.com>
2021-05-27 06:14:11 +0600

    Done for today may 27 6:14 created log,help with docstring , uninstall

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ sudo cp fb /usr/bin/
 sudo chgrp $(whoami) /usr/bin/fb
 sudo chmod 750 /usr/bin/fb
 echo "Installation Succesful"
+sudo mkdir /usr/bin/fb_shell_working_directory
+sudo chgrp $(whoami) /usr/bin/fb_shell_working_directory
+sudo chown $(whoami) /usr/bin/fb_shell_working_directory
+sudo chmod 770 /usr/bin/fb_shell_working_directory
 echo "run fb to run Shell Facebook"
