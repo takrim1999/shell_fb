2ea3220aa3f7ef49454ab929d6266782ac26342a
takrim <takrimulalom@gmail.com>
2021-05-27 19:52:57 +0600

    added update option and fixed log issues

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ sudo mkdir /usr/bin/fb_shell_working_directory
 sudo chgrp $(whoami) /usr/bin/fb_shell_working_directory
 sudo chown $(whoami) /usr/bin/fb_shell_working_directory
 sudo chmod 770 /usr/bin/fb_shell_working_directory
+sudo echo $(pwd) > /usr/bin/fb_shell_working_directory/install_info.txt
+sudo echo $(date) >> /usr/bin/fb_shell_working_directory/install_info.txt
 echo "run fb to run Shell Facebook"
